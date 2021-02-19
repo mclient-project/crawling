05232e4c16028dffff761e81ddd408d317f4cf16
Lisa Wenige <wenige@infai.org>
2021-02-19 12:39:46 +0100

    small fix

--- install-dcat.sh
+++ install-dcat.sh
@@ -3,4 +3,4 @@ git clone https://github.com/SmartDataAnalytics/dcat-suite.git
 git checkout develop
 mvn clean install -DskipTests=true
 ./reinstall-debs.sh
-cp /usr/local/bin/dcat /usr/bin/dcat
+cp /usr/bin/dcat /usr/local/bin/dcat 
